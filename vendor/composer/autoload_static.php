d2c8aaa95e8c6b42cadbbf521ef01e6edc7d1de9
mtemmar <mfa.temmar@gmail.com>
2019-06-15 15:17:23 +0200

    node modules + procfile

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -398,6 +398,7 @@ class ComposerStaticInitc15dd7bca13243be3bb3a7b48f3609ff
         'App\\Http\\Middleware\\TrimStrings' => __DIR__ . '/../..' . '/app/Http/Middleware/TrimStrings.php',
         'App\\Http\\Middleware\\TrustProxies' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustProxies.php',
         'App\\Http\\Middleware\\VerifyCsrfToken' => __DIR__ . '/../..' . '/app/Http/Middleware/VerifyCsrfToken.php',
+        'App\\Policies\\AssignmentPolicy' => __DIR__ . '/../..' . '/app/Policies/AssignmentPolicy.php',
         'App\\Providers\\AppServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AppServiceProvider.php',
         'App\\Providers\\AuthServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AuthServiceProvider.php',
         'App\\Providers\\BroadcastServiceProvider' => __DIR__ . '/../..' . '/app/Providers/BroadcastServiceProvider.php',
